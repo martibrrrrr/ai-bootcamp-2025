d63ed7bb05cc9ffa85facdefd1efea51d37ef61b
Martina Bruno <martinabruno027@gmail.com>
2025-01-28 16:59:54 +0100

    es 3

--- python/giorno2_3/esercizi/classi/main.py
+++ python/giorno2_3/esercizi/classi/main.py
@@ -1,46 +1,39 @@
+# Define the City class
+class City:
+    def __init__(self, name, pop):
+        self.name = name  # Name of the city
+        self.pop = pop  # Population of the city
 
-
-class Country:
-    def __init__(self, name):
-        self.name=name
-        self.regions= []
-    def add(self,region): #method. A function defined in a class
-        if isinstance(region,Region):
-            self.regions.append(region)
-        else:
-            raise ValueError
-
-    @property #to evaluate something
-    def pop(self):
-        """The total population of this country"""
-        for region in self.region:
-            region.pop += 1
-        return region.pop
-
-    """ def most_populous_city(self):
-        for region in self.regions:
-            for city in region.cities:
-                cities = city
-"""
-
-
+# Define the Region class
 class Region:
-    def __init__(self,name):
-        self.name = name
-        self.cities = []
-    def add(self,city):
-        if isinstance(city,City):
-            self.cities.append(city)
-        else:
-            raise ValueError
+    def __init__(self, name):
+        self.name = name  # Name of the region
+        self.cities = []  # List to store cities in the region
+
+    def add(self, city):
+        # Add a city to the region
+        self.cities.append(city)
 
     @property
     def pop(self):
-        for city in self.cities:
-            city.pop += 1
-class City:
-    """A city"""
+                return sum(city.pop for city in self.cities) # Calculate the total population of the region
 
-    def __init__(self, name, pop=None):
-        self.name=name
-        self.pop = 0
+# Define the Country class
+class Country:
+    def __init__(self, name):
+        self.name = name  # Add name of the country
+        self.regions = []  # List to store regions in the country
+
+    def add(self, region):
+            self.regions.append(region)# Add a region to the country
+
+    @property
+    def pop(self):
+                return sum(region.pop for region in self.regions) # Calculate the total population of the country
+
+    @property
+    def most_populuous_city(self):
+        # Find the city with the largest population in the country
+        all_cities = [city for region in self.regions for city in region.cities]
+        return max(all_cities, key=lambda city: city.pop) if all_cities else None
+
