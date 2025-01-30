9bcb6d32498b558427fcb0973a21b44191ef7823
Martina Bruno <martinabruno027@gmail.com>
2025-01-30 16:31:38 +0100

    es. 4

--- python/giorno4/main.py
+++ python/giorno4/main.py
@@ -1,88 +1,53 @@
-#assert [el.phone for el in directory.query(name="Vedrai")] == ["+39-333-333333"]
-#assert [el.phone for el in directory.query(name="Margaret")] == ["01-234-567"]
-#assert [el.phone for el in directory.find("Hamilton")] == ["01-234-567", None]
-#assert [el.name for el in directory.find("333")] == ["Vedrai"]
-
 class Directory:
     def __init__(self):# Implement qui il codice
-        self.contacts= [] #inizializzo la lista che memorizza le voci in rubrica senza contatti
-
+        self.contacts= [] #initialize the list that save contact Items in the Directory as empty
     def __len__(self):
-        return len(self.contacts) #calcolo il n di elementi nella voce self.contact
+        return len(self.contacts) #calculation of the number of elements  present in the item self.contact
 
     def add(self,contact): #aggiungo person o business
         self.contacts.append(contact)
 
     def query(self,name):
-        res = []
+        result_c = []
         for contact in self.contacts:
             if contact.name == name:
-                res.append(contact) #creo una lista. aggiungo il contatto alla lista
-        return res #lista contatti
+                result_c.append(contact) #contact list. I add the contact to the list
+        return result_c #contact list
 
     def find(self,search_contact):
-        result = []
+        result_c = []
+        #i add this in case i'm looking for contacts without number
         for contact in self.contacts:
-            if isinstance(contact, Person): #controllo l'istanza della classe: Person o Business
-                if search_contact in contact.name or search_contact in contact.surname or search_contact in contact.phone:
-                    result.append(contact)
-            elif isinstance(contact,Business):
-                if search_contact in contact.name or search_contact in contact.phone:
-                    result.append(contact)
-        return [self._get_phone(contact) for contact in result]
+            if search_contact == "":
+                #if not contact.phone:
+                if contact.phone is None or contact.phone == "":
+                    result_c.append(contact)  #Returns people with no number ""
 
-    def _get_phone(self, contact):
-        return contact.phone if contact.phone else None
+            elif isinstance(contact, Person):
+                if search_contact in contact.name or (contact.surname and search_contact in contact.surname):
+                    result_c.append(contact)  #We return the item `Person`
+                elif contact.phone and search_contact in contact.phone:
+                    result_c.append(contact)  #If we are looking for a number, we return the entire contact
+
+            elif isinstance(contact, Business):
+                if search_contact in contact.name or (contact.phone and search_contact in contact.phone):
+                    result_c.append(contact)  #If it is a company, return the name
+        return result_c
 
 class Person:
-    def __init__(self,name,surname,phone = ""):
+    def __init__(self,name,surname,phone=""):
         self.name=name
         self.surname=surname
-        self.phone = phone
+        self.phone = phone if phone else None #register as None ( inside phone object ) if i don't have the number phone (contact)
 
 class Business:
-    def __init__(self, name, phone= ""):
-        self.name = name
-        self.phone = phone
-
-#directory = Directory()
-#directory.add(Person(name="Margaret", surname="Hamilton", phone="01-234-567"))
-#directory.add(Business(name="Vedrai", phone="+39-333-333333"))
-#directory.add(Person(name="Linda", surname="Hamilton"))
-#assert len(directory) == 3
-
-"""   class Entry:
     def __init__(self, name, phone=""):
         self.name = name
-        self.phone = phone
-    def matches(self, keyword):
-        return keyword in self.name or (self.phone and keyword in self.phone)
+        self.phone = phone if phone else None #None - inside phone object -
 
-class Person(Entry):
-    def __init__(self, name, surname, phone=None):
-        super().__init__(name, phone)
-        self.surname = surname
 
-    def matches(self, keyword):
-        return super().matches(keyword) or keyword in self.surname
-
-class Business(Entry):
-    def __init__(self, name, phone):
-        super().__init__(name, phone)
-
-class Directory:
-    def __init__(self):
-        self.entries = []
-
-    def __len__(self):
-        return len(self.entries)
-
-    def add(self, entry):
-        self.entries.append(entry)
-
-    def query(self, name):
-        return [entry for entry in self.entries if entry.name == name]
-
-    def find(self, keyword):
-        return [entry for entry in self.entries if entry.matches(keyword)]
- """
+##ADDITIONAL CONTROLS. PASS THIS TO test.py file
+##result = [el.name for el in directory.find("")]
+##print(f"Risultato attuale di find(\"\"): {result}") #print for test "find a empty number phone"
+#assert [el.name for el in directory.find("")] == ["Linda"]
+#assert [el.phone for el in directory.find("Margaret")] == ["01-234-567"]
--- python/giorno4/tests.py
+++ python/giorno4/tests.py
@@ -12,9 +12,12 @@ assert len(directory) == 3
 
 # Domanda: perché usiamo le list comprehension?
 # Risposta: perchè possiamo avere risultati multipli
-assert [el.phone for el in directory.query(name="Vedrai")] == ["+39-333-333333"]
 assert [el.phone for el in directory.query(name="Margaret")] == ["01-234-567"]
+assert [el.phone for el in directory.query(name="Vedrai")] == ["+39-333-333333"]
 assert [el.phone for el in directory.find("Hamilton")] == ["01-234-567", None]
 assert [el.name for el in directory.find("333")] == ["Vedrai"]
 
-
+result = [el.name for el in directory.find("")]
+print(f"Risultato attuale di find(\"\"): {result}") #print for test "find a empty number phone"
+assert [el.name for el in directory.find("")] == ["Linda"]
+assert [el.phone for el in directory.find("Margaret")] == ["01-234-567"]
