5614323b2cad447f2c4b4305837cc80fdb2ab4ca
Martina Bruno <martinabruno027@gmail.com>
2025-02-03 17:04:00 +0100

    es0 giorno7

--- python/giorno7/ex0/main.py
+++ python/giorno7/ex0/main.py
@@ -1,6 +1,25 @@
 import csv
 
-with open("data.csv") as fd:
-    reader = csv.reader(fd)
+def surname(riga):
+    # funzione f per restituire il secondo elemento della tupla (cognome).
+    return riga[1]
+
+with open("data.csv") as fd: #associo il file importato alla variabile fd
+    reader = csv.reader(fd) #creo oggetto reader che itera sulle righe del file .csv
     for line in reader:
         print(line)
+    #con "with open" chiudo file automaticamente una volta terminata la lettura
+
+with open("data.csv", mode="r") as fd:
+    reader = csv.reader(fd)
+    header = next(reader) #per leggere l'intestazione così la rimovo dal testo che leggo successivamente
+    data = list(reader)  # converto il testo in una lista
+
+reorder_data = (sorted(data,key=surname))
+print("Reorder Data:")
+for index, line in enumerate(reorder_data, start = 1):
+    print([index] + line)
+with open("data2.csv", mode = "w") as fd:
+    writer=csv.writer(fd)
+    writer.writerow(header) #aggiungo l'intestazione
+    writer.writerows(reorder_data)
